ff2cbe25184f493c7560b4c71d966ec7cb2ad500
DevBerriel <134451901+DevBerriel@users.noreply.github.com>
2023-10-11 17:19:47 -0300

    exercicios lambda pt3

---
+++ src/lambdas/Produto.java
@@ -0,0 +1,13 @@
+package src.lambdas;
+
+public class Produto extends Object {
+    final String nome;
+    final double preco;
+    final double desconto;
+
+    public Produto(String nome, double preco, double desconto) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+}
